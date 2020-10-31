db9827dc35cac708f23a18550ea84f21ae6d278a
andreguedes <andresguedes@live.com>
2020-10-31 00:05:37 -0300

    Add base activity

---
+++ app/src/main/java/br/com/gifs/ui/GifsActivity.kt
@@ -0,0 +1,15 @@
+package br.com.gifs.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.gifs.R
+
+class GifsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_gifs)
+    }
+
+}
